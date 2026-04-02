1d6c37e5b8b1ee0e1a602036c3dce79006ef0eb7
weswc <wes@a2-ai.com>
2026-04-02 17:02:15 -0400

    stash review

--- astro.config.mjs
+++ astro.config.mjs
@@ -128,6 +128,7 @@ export default defineConfig({
 						{ label: 'QC Record', slug: 'concepts/record' },
 						{ label: 'Authentication', slug: 'concepts/authentication' },
 						{ label: 'QC Status', slug: 'concepts/qc-status' },
+						{ label: 'Review Stashes', slug: 'concepts/review-stashes' },
 						{ label: 'Unapproval Impact', slug: 'concepts/unapproval-impact' },
 					]
 				},
